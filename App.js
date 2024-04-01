bbe8e197105c95c9cd14741cb313117927c48938
Omkar Ubale <oubale@gmail.com>
2024-04-01 17:06:06 -0400

    A5: 3.0 setup - created Lab routes

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import express from "express";
+import Hello from "./Hello.js";
+import Lab5 from "./Lab5.js";
+
 const app = express();
-app.get("/hello", (req, res) => {
-  res.send("Life is good!");
-});
-app.get("/", (req, res) => {
-  res.send("Welcome to Full Stack Development!");
-});
+Hello(app);
+Lab5(app);
+
 app.listen(4000);
