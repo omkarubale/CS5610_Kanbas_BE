a8419b9103b9ee5c20be5be129ac6eec6803b9b9
Omkar Ubale <oubale@gmail.com>
2024-04-02 00:44:39 -0400

    A5: 3.4 - completed Receiving data from server as HTTP Responses

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
 import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import cors from "cors";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 Hello(app);
 Lab5(app);
 
