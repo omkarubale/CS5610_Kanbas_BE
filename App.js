816f40d20745f0b8e281604655d34f8e92b52d84
Omkar Ubale <oubale@gmail.com>
2024-04-03 02:36:46 -0400

    A5: 4.2 - Courses Routes added

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+CourseRoutes(app);
 Hello(app);
 Lab5(app);
 
