44f20b5ccb8a22965c9c74df193afa9441c79a19
Omkar Ubale <oubale@gmail.com>
2024-04-14 03:50:29 -0400

    A6: 3.5.2 completed - added profile, sign in endpoints

--- App.js
+++ App.js
@@ -6,10 +6,20 @@ import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import QuizRoutes from "./Kanbas/quizzes/routes.js";
+import UserRoutes from "./Users/routes.js";
+import mongoose from "mongoose";
 
+const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_USERNAME = process.env.MONGODB_USERNAME;
+const MONGODB_PASSWORD = process.env.MONGODB_PASSWORD;
+
+mongoose.connect(
+  `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@${MONGODB_URL}`
+);
 const app = express();
 app.use(express.json());
 app.use(cors());
+UserRoutes(app);
 CourseRoutes(app);
 ModuleRoutes(app);
 AssignmentRoutes(app);
