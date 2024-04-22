105ec72e625dbe911c08eff63ea9f3e78fd310af
Omkar Ubale <oubale@gmail.com>
2024-04-22 03:44:29 -0400

    FEATURE: completed updateQuizQuestions endpoint

--- Kanbas/quizQuestions/dao.js
+++ Kanbas/quizQuestions/dao.js
@@ -1,21 +1,24 @@
-import mongoose from "mongoose";
 import { quizQuestionsModel } from "./model.js";
 
 export const getQuizQuestionsByQuizId = (quizId) =>
   quizQuestionsModel.find({ quizId: quizId });
 
-export const createQuizQuestion = (quizId, quizQuestion) => {
+export const createQuizQuestion = async (quizId, quizQuestion) => {
   delete quizQuestion._id;
   quizQuestion.quizId = quizId;
-  return quizQuestionsModel.create(quizQuestion);
+  return await quizQuestionsModel.create(quizQuestion);
 };
 
-export const deleteQuizQuestion = (quizQuestionId) => {
-  quizQuestionsModel.findByIdAndDelete(quizQuestionId);
+export const deleteQuizQuestion = async (quizQuestionId) => {
+  return await quizQuestionsModel.findByIdAndDelete(quizQuestionId);
 };
 
-export const updateQuizQuestion = (quizQuestionId, quizQuestion) =>
-  quizQuestionsModel.findByIdAndUpdate(quizQuestionId, quizQuestion);
+export const updateQuizQuestion = async (quizQuestionId, quizQuestion) => {
+  return await quizQuestionsModel.findByIdAndUpdate(
+    quizQuestionId,
+    quizQuestion
+  );
+};
 
 export const getQuizQuestion = (quizQuestionId) =>
   quizQuestionsModel.findById(quizQuestionId);
--- Kanbas/quizQuestions/routes.js
+++ Kanbas/quizQuestions/routes.js
@@ -56,9 +56,73 @@ export default function QuizRoutes(app) {
     }
   };
 
+  const updateQuizQuestions = async (req, res) => {
+    try {
+      const { qid } = req.params;
+      const newQuizQuestions = req.body;
+      const oldQuizQuestions = await dao.getQuizQuestionsByQuizId(qid);
+
+      const promises = [];
+
+      // create and update
+      for (let i = 0; i < newQuizQuestions.length; i++) {
+        let matchFound = false;
+
+        for (let j = 0; j < oldQuizQuestions.length; j++) {
+          if (newQuizQuestions[i]._id === oldQuizQuestions[j]._id.toString()) {
+            // update
+            matchFound = true;
+            promises.push(
+              await dao.updateQuizQuestion(
+                newQuizQuestions[i]._id,
+                newQuizQuestions[i]
+              )
+            );
+            break;
+          }
+        }
+
+        if (matchFound === false) {
+          // create
+          delete newQuizQuestions[i]._id;
+          promises.push(
+            await dao.createQuizQuestion(
+              newQuizQuestions[i].quizId,
+              newQuizQuestions[i]
+            )
+          );
+        }
+      }
+
+      // delete
+      for (let j = 0; j < oldQuizQuestions.length; j++) {
+        let matchFound = false;
+        for (let i = 0; i < newQuizQuestions.length; i++) {
+          if (newQuizQuestions[i]._id === oldQuizQuestions[j]._id.toString()) {
+            matchFound = true;
+            break;
+          }
+        }
+
+        if (matchFound === false) {
+          // delete
+          promises.push(
+            await dao.deleteQuizQuestion(oldQuizQuestions[j]._id.toString())
+          );
+        }
+      }
+
+      await Promise.all(promises);
+      res.json(204);
+    } catch (e) {
+      console.log("Error: ", e);
+    }
+  };
+
   app.get("/api/quizzes/:qid/questions", findQuizQuestionsByQuizId);
   app.post("/api/quizzes/:qid/quizQuestion", createQuizQuestion);
   app.get("/api/quizQuestions/:qqid", findQuizQuestion);
   app.delete("/api/quizQuestions/:qqid", deleteQuizQuestion);
   app.put("/api/quizQuestions/:qqid", updateQuizQuestion);
+  app.post("/api/quizzes/:qid/quizQuestions/bulkEdit", updateQuizQuestions);
 }
